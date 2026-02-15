7f0b5419d66a88ee9b069e71586c689a3456eabf
LizardKing <lizardking131313@gmail.com>
2026-02-15 23:07:53 +0300

    Tailwind

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -2,5 +2,12 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <div class="bg-brand-dark text-surface-base min-h-screen">
+      <div class="mx-auto max-w-5xl p-6">
+        <div class="bg-surface-base text-brand-dark rounded-xl p-6">
+          <button class="bg-brand-red text-surface-base rounded-xl px-6 py-3">OK</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,7 +1,20 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// noinspection JSUnusedGlobalSymbols
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      // @ts-ignore
+      tailwindcss(),
+    ],
+  },
+
   runtimeConfig: {
     public: {
       directusUrl: process.env.NUXT_PUBLIC_DIRECTUS_URL,
