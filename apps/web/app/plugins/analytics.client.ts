a9097be8033494b2d59de9ca213f516a027a683f
LizardKing <lizardking131313@gmail.com>
2026-03-16 13:54:00 +0300

    fix: quiz modal

--- apps/web/app/plugins/analytics.client.ts
+++ apps/web/app/plugins/analytics.client.ts
@@ -50,39 +50,11 @@ const setupYandexMetrika = (counterId: string): void => {
   )
 }
 
-const setupRoistat = (projectId: string, host: string): void => {
-  appendInlineScript(
-    'roistat-counter-loader',
-    `
-      (function(w, d, s, h, id) {
-        w.roistatProjectId = id
-        w.roistatHost = h
-        var protocol = d.location.protocol === 'https:' ? 'https://' : 'http://'
-        var source = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie)
-          ? '/dist/module.js'
-          : '/api/site/1.0/' + id + '/init?referrer=' + encodeURIComponent(d.location.href)
-        var scriptElement = d.createElement(s)
-        scriptElement.charset = 'UTF-8'
-        scriptElement.async = 1
-        scriptElement.src = protocol + h + source
-        var firstScript = d.getElementsByTagName(s)[0]
-        firstScript.parentNode.insertBefore(scriptElement, firstScript)
-      })(window, document, 'script', '${host}', '${projectId}')
-    `
-  )
-}
-
 export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig()
   const metrikaId = runtimeConfig.public.yandexMetrikaId
-  const projectId = runtimeConfig.public.roistatProjectId
-  const host = runtimeConfig.public.roistatHost
 
   if (metrikaId) {
     setupYandexMetrika(metrikaId)
-  }
-
-  if (projectId && host) {
-    setupRoistat(projectId, host)
   }
 })
